d7e51910905fa0523115dbc76b32908ab5c6a5fd
SimonaLazzarini <simona18397@gmail.com>
2018-07-19 12:19:44 +0200

    Caricamento package interfacciaGrafica
    Questo package contiene i file per gestire la prima fase, ossia: schermata iniziale e menù principale. Per ora si tratta di schermate banali e da modificare.

---
+++ src/interfacciaGrafica/titoloController.java
@@ -0,0 +1,28 @@
+package interfacciaGrafica;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.StackPane;
+
+public class titoloController implements Initializable {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private StackPane root;
+
+    @FXML
+    void initialize() {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
